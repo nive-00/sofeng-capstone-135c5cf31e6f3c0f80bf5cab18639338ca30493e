d04b891b3d20e9b74d3f00b5ebdf1f5fdcaff73f
nive-00 <130848418+nive-00@users.noreply.github.com>
2023-06-26 06:41:14 +0800

    reg

--- users/forms.py
+++ users/forms.py
@@ -7,4 +7,4 @@ class UserRegisterForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1','password2']
+        fields = ['user','email','password1','password2']
