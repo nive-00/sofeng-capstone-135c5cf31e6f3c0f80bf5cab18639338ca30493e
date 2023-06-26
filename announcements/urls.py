a48aa02871fbca0e3d6e8dff70c9e6084ab10592
nive-00 <130848418+nive-00@users.noreply.github.com>
2023-06-26 15:36:01 +0800

    almost finished

--- announcements/urls.py
+++ announcements/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='announcements-home'),
+    path('', PostListView.as_view(), name='announcements-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('about/', views.about, name='announcements-about'),
 ]
--- announcements/views.py
+++ announcements/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Post
 
 def home(request):
@@ -6,6 +8,31 @@ def home(request):
         'posts' : Post.objects.all()
     }
     return render(request, 'announcements/home.html',context)
+
+class PostListView(ListView):   
+    model = Post
+    template_name = 'announcements/home.html'
+    context_object_name = 'posts'
+    order = ['-date_posted']
+    
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title','content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user 
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ['title','content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user 
+        return super().form_valid(form)
 
 def about(request):
     return render(request, 'announcements/about.html', {'title':'hello'})
